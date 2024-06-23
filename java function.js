88bd7c7fce3325ebd751918c9801bd5e711c9b6b
rajia sultana shraboni <shraboni3103@gmail.com>
2024-06-23 23:02:40 +0600

    update

---
+++ java function.js
@@ -0,0 +1,31 @@
+// function Fun(){
+//     var num = 20;
+//     var num2 = 20;
+//     console.log(num * num2);
+
+
+// }
+
+// Fun()
+
+
+
+// function Fun (num1, num2){
+//     var total = num1 + num2;
+//     console.log(total)
+// }
+
+// Fun(20, 30);
+// Fun(20, 40);
+// Fun(20, 50);
+// Fun(20, 80);
+
+
+function fun (num1){
+    var total = num1 * 10;
+    return total;
+}
+
+var a = fun(10);
+var b = fun(20);
+console.log (a + b)
--- while loop.js
+++ while loop.js
@@ -2,7 +2,7 @@ var i = 0;
 
 
 while(i < 10 ){
-    console.log("hello world");
+    console.log(i);
     i++
 
 }
